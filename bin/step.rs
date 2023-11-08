a03f9ea9aba0e01b0e6ab750d6ad4e22c488544d
ratan kaliani <ratan@succinct.xyz>
2023-11-07 23:31:25 -0800

    set validator_set_size_max in bin, don't use env

--- bin/step.rs
+++ bin/step.rs
@@ -14,24 +14,10 @@
 //!
 //!
 //!
-use std::env;
-
 use plonky2x::backend::function::Plonky2xFunction;
 use tendermintx::step::StepCircuit;
 
 fn main() {
-    let env_validator_set_size_max = env::var("VALIDATOR_SET_SIZE_MAX").unwrap_or(0.to_string());
-
-    if env_validator_set_size_max == 128.to_string() {
-        const VALIDATOR_SET_SIZE_MAX: usize = 128;
-        StepCircuit::<VALIDATOR_SET_SIZE_MAX>::entrypoint();
-    } else if env_validator_set_size_max == 32.to_string() {
-        const VALIDATOR_SET_SIZE_MAX: usize = 32;
-        StepCircuit::<VALIDATOR_SET_SIZE_MAX>::entrypoint();
-    } else if env_validator_set_size_max == 4.to_string() {
-        const VALIDATOR_SET_SIZE_MAX: usize = 4;
-        StepCircuit::<VALIDATOR_SET_SIZE_MAX>::entrypoint();
-    } else {
-        panic!("VALIDATOR_SET_SIZE_MAX must be set to 128, 32 or 4");
-    }
+    const VALIDATOR_SET_SIZE_MAX: usize = 100;
+    StepCircuit::<VALIDATOR_SET_SIZE_MAX>::entrypoint();
 }
