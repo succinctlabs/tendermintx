f68843c1468ac10e76cdd9ad371db523449eaa17
Ratan Kaliani <ratankaliani@berkeley.edu>
2023-11-10 10:33:44 -0800

    feat(script): add Succinct X api key (#12)

--- bin/tendermintx.rs
+++ bin/tendermintx.rs
@@ -52,7 +52,8 @@ impl TendermintXOperator {
         let data_fetcher = InputDataFetcher::new(&tendermint_rpc_url, "");
 
         let succinct_rpc_url = env::var("SUCCINCT_RPC_URL").expect("SUCCINCT_RPC_URL must be set");
-        let client = SuccinctClient::new(succinct_rpc_url);
+        let succinct_api_key = env::var("SUCCINCT_API_KEY").expect("SUCCINCT_API_KEY must be set");
+        let client = SuccinctClient::new(succinct_rpc_url, succinct_api_key);
 
         Self {
             config,
